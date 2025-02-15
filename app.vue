ed833d21f665a0de7b982444648e2e907d6bf267
Marta Kochjarova <kochjarova4@uniba.sk>
2025-02-15 14:01:21 +0100

    Initial commit: Basic Nuxt.js setup

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useHead } from '#imports'
+
+useHead({
+  title: 'TrackerApp',
+  meta: [
+    { name: 'description', content: 'This is my Nuxt app description' }
+  ]
+})
+</script>
+
+<template>
+  <h1>aaa</h1>
+</template>
